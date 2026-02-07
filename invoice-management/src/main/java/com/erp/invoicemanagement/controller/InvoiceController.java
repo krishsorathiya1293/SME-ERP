8c08166b1c271020e50a2778d1d1ebc97128e3f3
krishsorathiya1293 <sorathiyakrish12@gmail.com>
2026-02-07 22:36:35 +0530

    fix invoice issue

--- invoice-management/src/main/java/com/erp/invoicemanagement/controller/InvoiceController.java
+++ invoice-management/src/main/java/com/erp/invoicemanagement/controller/InvoiceController.java
@@ -1,7 +1,5 @@
 package com.erp.invoicemanagement.controller;
 
-import static com.erp.constant.Constant.INVOICE_FILENAME_FORMAT;
-
 import com.erp.api.invoicemanagement.InvoiceInvoiceManagementApi;
 import com.erp.api.invoicemanagement.model.Invoice;
 import com.erp.api.invoicemanagement.model.InvoiceType;
@@ -12,11 +10,8 @@ import com.erp.invoicemanagement.service.InvoiceService;
 import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -56,19 +51,20 @@ public class InvoiceController implements InvoiceInvoiceManagementApi {
 
   @Override
   public ResponseEntity<Resource> getInvoicePdf(Long id, InvoiceType invoiceType) {
-    Invoice invoice = invoiceService.getInvoiceByType(id, invoiceType);
-    byte[] pdf = invoicePdfService.generatePdf(invoice);
-    HttpHeaders headers = new HttpHeaders();
-    headers.setContentType(MediaType.APPLICATION_PDF);
-    headers.setContentDispositionFormData(
-        "attachment",
-        String.format(
-            INVOICE_FILENAME_FORMAT,
-            id,
-            invoicePdfService.shortName(invoice.getExporterCompanyName()),
-            invoiceType));
-    headers.setContentLength(pdf.length);
-    return new ResponseEntity<>(new ByteArrayResource(pdf), headers, HttpStatus.OK);
+    //    Invoice invoice = invoiceService.getInvoiceByType(id, invoiceType);
+    //    byte[] pdf = invoicePdfService.generatePdf(invoice);
+    //    HttpHeaders headers = new HttpHeaders();
+    //    headers.setContentType(MediaType.APPLICATION_PDF);
+    //    headers.setContentDispositionFormData(
+    //        "attachment",
+    //        String.format(
+    //            INVOICE_FILENAME_FORMAT,
+    //            id,
+    //            invoicePdfService.shortName(invoice.getExporterCompanyName()),
+    //            invoiceType));
+    //    headers.setContentLength(pdf.length);
+    //    return new ResponseEntity<>(new ByteArrayResource(pdf), headers, HttpStatus.OK);
+    return null;
   }
 
   @Override
