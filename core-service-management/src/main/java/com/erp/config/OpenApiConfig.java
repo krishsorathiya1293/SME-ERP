ef3c0fd15f0b053a82a2e9c5de17fdc02973c57f
krishsorathiya1293 <sorathiyakrish12@gmail.com>
2026-03-31 21:10:53 +0530

    Fix Workflow

--- core-service-management/src/main/java/com/erp/config/OpenApiConfig.java
+++ core-service-management/src/main/java/com/erp/config/OpenApiConfig.java
@@ -141,4 +141,17 @@ public class OpenApiConfig {
       return data;
     }
   }
+
+  @RestController
+  @RequestMapping("/doc/v3/gres")
+  public class GresDocumentationController {
+    @GetMapping
+    public String getOpenApiYaml() throws IOException {
+      String data = "";
+      ClassPathResource cpr = new ClassPathResource("gres-management.yaml");
+      byte[] bdata = FileCopyUtils.copyToByteArray(cpr.getInputStream());
+      data = new String(bdata, StandardCharsets.UTF_8);
+      return data;
+    }
+  }
 }
